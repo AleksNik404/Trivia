de94e7336c0bcf6e82384cbf5918c300d637ece2
AleksNik404 <AleksNik404@gmail.com>
2023-02-07 13:11:34 +0300

    feat: tag entity and pg database

--- 2.Kocherhin/nest_medium_clone/src/ormconfig.ts
+++ 2.Kocherhin/nest_medium_clone/src/ormconfig.ts
@@ -1,12 +1,16 @@
 import { DataSourceOptions } from 'typeorm';
+import { TagEntity } from './tag/tag.entity';
 
 const options: DataSourceOptions = {
   type: 'postgres',
   host: 'localhost',
   port: 5432,
-  username: 'mediumclone',
+  username: 'medclone',
   password: '2808',
-  database: 'mediumclone',
+  database: 'medclone',
+
+  entities: [TagEntity],
+  synchronize: true,
 };
 
 export default options;
--- 2.Kocherhin/nest_medium_clone/src/tag/tag.module.ts
+++ 2.Kocherhin/nest_medium_clone/src/tag/tag.module.ts
@@ -1,8 +1,11 @@
 import { TagService } from './tag.service';
 import { TagController } from './tag.controller';
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagEntity } from './tag.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
   providers: [TagService],
 })
